beb7bd48cc5411f70845465dacd39a26f895cc14
William Fleurant <william@netblazr.com>
2018-08-16 02:40:38 +0000

    configure-gps: obey systemd heh :/

--- scripts/configure-gps.sh
+++ scripts/configure-gps.sh
@@ -5,7 +5,7 @@ sudo systemctl stop serial-getty@ttyS0.service
 sudo systemctl disable serial-getty@ttyS0.service
 
 ## Install gps-daemon and all gpsd-clients
-sudo apt-get update
+sudo apt-get update 
 sudo apt-get install -y gpsd gpsd-clients python-gps
 
 ## Pass gps-device '/dev/ttyS0' to gps-daemon
@@ -14,19 +14,19 @@ sudo sed -i "s/^\(GPSD_OPTIONS\)=.*/GPSD_OPTIONS=\"\/dev\/ttyS0\"/g" /etc/defaul
 ## Restart gps-daemon
 for x in enable restart; do sudo systemctl $x gpsd.socket; done
 
-## Enable UART in boot config if missing
-grep -q "enable_uart=0" /boot/config.txt
+## Append UART in boot config if missing, else enable if found
+grep -q "enable_uart=1" /boot/config.txt
 if [ $? -eq 1 ]; then
   echo enable_uart=1 | sudo tee -a /boot/config.txt
 else
-  :
+  sudo sed -i "s/^\(enable_uart\)=.*/enable_uart=1/g" /boot/config.txt
 fi
 
-exit
-
-## Alternative Method:
-# raspi-config
+## Alternative UART/Serial method via: raspi-config
 ### interfaces options
 ###  Serial
 ###   No
 ###    Yes
+
+sync && exit 0
+
